55a219f8900072cc8d101ac7eaae6484b0d5f83d
Argeu Souza <argeusousaptbr@gmail.com>
2026-05-26 10:18:34 -0300

    feat: 1 - Um novo projeto utilizando o Spring Framework

--- src/main/java/br/com/grp/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/grp/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,26 @@
 package br.com.grp.screenmatch;
 
+import br.com.grp.screenmatch.model.DadosSerie;
+import br.com.grp.screenmatch.service.ConsumoApi;
+import br.com.grp.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=3b92e6d");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
 }
