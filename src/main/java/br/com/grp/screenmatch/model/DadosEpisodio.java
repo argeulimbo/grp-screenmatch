3d974729484e9b75901c4dcba9fa5b3baf551aed
Argeu Souza <argeusousaptbr@gmail.com>
2026-05-27 10:52:20 -0300

    feat: 03 - Manipulando com fluxos as coleções de dados - Final

--- src/main/java/br/com/grp/screenmatch/model/DadosEpisodio.java
+++ src/main/java/br/com/grp/screenmatch/model/DadosEpisodio.java
@@ -9,4 +9,19 @@ public record DadosEpisodio(
         @JsonAlias("Episode") Integer numero,
         @JsonAlias("imdbRating") String avaliacao,
         @JsonAlias("Released") String dataLancamento) {
+
+    public DadosEpisodio(String titulo, Integer numero, String avaliacao, String dataLancamento) {
+        this.titulo = titulo;
+        this.numero = numero;
+        if (avaliacao == null) {
+            this.avaliacao = "";
+        } else {
+            this.avaliacao = avaliacao;
+        }
+        if (dataLancamento == null) {
+            this.dataLancamento = "";
+        } else {
+            this.dataLancamento = dataLancamento;
+        }
+    }
 }
--- src/main/java/br/com/grp/screenmatch/principal/Principal.java
+++ src/main/java/br/com/grp/screenmatch/principal/Principal.java
@@ -7,6 +7,8 @@ import br.com.grp.screenmatch.model.Episodio;
 import br.com.grp.screenmatch.service.ConsumoApi;
 import br.com.grp.screenmatch.service.ConverteDados;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -59,6 +61,21 @@ public class Principal {
                 ).collect(Collectors.toList());
 
         episodios.forEach(System.out::println);
+
+        System.out.println("A partir de que ano você deseja ver os episódios? ");
+        var ano = sc.nextInt();
+        sc.nextLine();
+
+        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+        DateTimeFormatter formatador =  DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                        "Temporada: " + e.getTemporada() +
+                                " Episódio: " + e.getTitulo() +
+                                " Data Lançamento: " + e.getDataLancamento().format(formatador)
+                ));
 
     }
 }
