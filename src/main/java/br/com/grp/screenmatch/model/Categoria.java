8d99f688d5ce343f2a6c5ee9abf53f11d9e17e7d
Argeu Souza <argeusousaptbr@gmail.com>
2026-05-29 00:22:32 -0300

    feat: Modelando categorias e séries

---
+++ src/main/java/br/com/grp/screenmatch/model/Categoria.java
@@ -0,0 +1,24 @@
+package br.com.grp.screenmatch.model;
+
+public enum Categoria {
+    COMEDIA("Comedy"),
+    ACAO("Action"),
+    ROMANCE("Romance"),
+    DRAMA("Drama"),
+    CRIME("Crime");
+
+    private String categoriaOmdb;
+
+    Categoria(String categoriaOmdb) {
+        this.categoriaOmdb = categoriaOmdb;
+    }
+
+    public static Categoria fromString(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Nenhuma categoria encontrada para a série");
+    }
+}
--- src/main/java/br/com/grp/screenmatch/model/Serie.java
+++ src/main/java/br/com/grp/screenmatch/model/Serie.java
@@ -2,6 +2,8 @@ package br.com.grp.screenmatch.model;
 
 import lombok.*;
 
+import java.util.OptionalDouble;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -10,12 +12,25 @@ public class Serie {
 
     private String titulo;
     private Integer totalTemporadas;
-    private String avaliacao;
-    private String genero;
+    private Double avaliacao;
+    private Categoria genero;
     private String atores;
     private String poster;
     private String sinopse;
 
+    public Serie(DadosSerie dadosSerie) {
+        this.titulo = dadosSerie.titulo();
+        this.totalTemporadas = dadosSerie.totalTemporadas();
+        try {
+            this.avaliacao = OptionalDouble.of(Double.valueOf(dadosSerie.avaliacao())).orElse(0);
+        } catch (NumberFormatException ex) {
+            this.avaliacao = 0.0;
+        }
+        this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
+        this.atores = dadosSerie.atores();
+        this.poster = dadosSerie.poster();
+        this.sinopse = dadosSerie.sinopse();
 
+    }
 
 }
